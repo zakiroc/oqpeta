9a8e643f3e56a16095f91d3764e79a74e66b4b0c
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-08-17 12:58:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,386 @@
+const data = {
+    aksiyalar: [
+        { img: "http://cc.oqtepalavash.uz/api/image/80fb7689-9762-4360-b89f-495fce396443.png", title: "5 tasi 25000", price: 25000 },
+      
+        { img: "http://cc.oqtepalavash.uz/api/image/66e86a42-b957-4a3f-a0be-adc035adf4e0.jpg", title: "11 ta xaggi pepsi 0,5", price: 47000 },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/6aa87b6c-44cb-4d2d-83e7-5bcd3ceedbc4.jpg",
+            title: "Shourma juftligi",
+            price: 85000,
+        },
+    ],
+};
+
+const barakalisetlar = {
+    barakaliSetlar: [
+       
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/13bb3c22-7f8d-4094-9480-36a502d7260a.png",
+            title: "Chiz da baraka",
+            price: 165000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/353b5b07-4b77-4a10-ba84-f8285c0332fc.jpg",
+            title: "Tandir juftlik lavashi",
+            price: 95000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/6aa87b6c-44cb-4d2d-83e7-5bcd3ceedbc4.jpg",
+            title: "Shourma juftligi",
+            price: 85000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/0721d107-3aa3-4378-bed1-75838414fb27.png",
+            title: "Pita doner seti",
+            price: 45000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/1d4fd8b2-0ff9-40f2-8851-0002f0c534b8.png",
+            title: "Lavash seti",
+            price: 50000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/275aa717-0b3f-4170-ba73-b1cb0afa39ae.png",
+            title: "Hot dog seti",
+            price: 30000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/a340204b-c8d6-454c-8166-df34dbc8f0d5.png",
+            title: "Shoxona hot dog seti",
+            price: 35000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/b80ebe20-9c5c-425e-a5c4-5683555847e9.png",
+            title: "Gambuger seti",
+            price: 40000,
+        },
+        
+      
+       
+        
+    ],
+};
+
+const yahnakofee = {
+    yaxnaKofe: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/6750bfc7-7231-40e7-9294-81f767613fd0.png",
+            title: "Yaxna shokolad kapuchino",  price: 25000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/9227f145-d408-486b-ae76-5c98d001f937.png",
+            title: "Yaxna tuzli karame kapuchino",
+            price: 25000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/b730a1ee-9b68-4979-ac9b-521e731c4bee.png",
+            title: "Yaxna iris tofi kapuchino",
+            price: 25000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/1c58506d-37c7-4546-8926-ead9724a212d.png",
+            title: "Yaxna klasik kapuchino",
+            price: 23000,
+        },
+    ],
+}
+
+const qarsildoq  ={
+    sendvichJojalar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/4b739127-a725-40ee-9c9a-d56f5ffd298d.png",
+            title: "Joja box",
+            price: 32000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/3d86d212-8967-44e6-9dfa-5249c9c3a67d.png",
+            title: "Klab sendvich frisiz",
+            price: 32000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/83753fef-a9f9-4295-b059-1e42391bc209.png",
+            title: "Klab sendvich fri bilan",
+            price: 36000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/63ef02ac-c5f8-4df4-b15f-5cb67a055256.png",
+            title: "Strips 5 dona",
+            price: 30000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/5dedbd5e-c782-48a5-b85e-e2cb6076077d.png",
+            title: "Strips 3 dona",
+            price: 19000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/939d446d-e185-4a71-a20c-ccde2f01d32d.png",
+            title: "Baytslar",
+            price: 21000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/0f9fb786-4cdc-46c5-b442-a3178ac93765.png",
+            title: "Naggets boks",
+            price: 34000,
+        },
+       
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/0ba7423f-afe6-4d9d-b29f-87db8e756a01.png",
+            title: "Naggetslar 5 dona",
+            price: 15000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/2538b5cd-1028-427f-918b-e6e18c6d0e86.png",
+            title: "Naggets 15 dona",
+            price: 36000,
+        },
+    ],
+}
+
+const lavashlar = {
+    lavashlar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/43be6508-dff0-4823-90ff-b3b0d3973743.png",
+            title: "Orginal katta lavash",
+            price: 32000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/37d593ff-6092-43bb-9a62-09cc8c3e0b8b.png",
+            title: "Orginal kichik lavash",
+            price: 27000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/4c496127-076b-4e18-91ee-48ab962fb21a.png",
+            title: "Pishloqli katta lavash",
+            price: 35000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/041f54ac-e0ff-4afd-bb30-26c7ebec7564.png",
+            title: "Pishloqli kichik lavash",
+            price: 30000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/3ae07ce8-dbf7-4420-986b-571c25ad424e.png",
+            title: "Tandir lavash",
+            price: 34000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/a87d3ca3-531e-47ae-95b2-86d14c0ec7a9.png",
+            title: "Pishloqli tandir lavash",
+            price: 37000,
+        },
+    ],
+}
+
+const bur = {
+    burgerHotDog: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/d9d553d9-ddb0-41cd-9523-ad67057beb4c.png",
+            title: "Hamburger",
+            price: 25000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/2c5b49c7-ed11-4219-a3f3-3725b1ddbf8d.png",
+            title: "Chizburger",
+            price: 27000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/51cbea60-9599-46b4-817b-a0583c7a303b.png",
+            title: "Big burger ",
+            price: 37000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/b045b8b3-6e32-46f3-bad0-0ad539c56937.png",
+            title: "Big chizburger",
+            price: 41000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/4ba84e47-e14f-475b-b1fc-ead298cfa2e3.png",
+            title: "Hot dog",
+            price: 14000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/818b8a47-fd7f-4164-8859-e7b9b4fb2fc3.png",
+            title: "Pishloqli hot dog",
+            price: 17000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/10b4be39-022f-4076-a563-ea3f23cefedb.png",
+            title: "Shohona hot dog",
+            price: 25000,
+        },
+    ],
+}
+const salatlar = {
+    salatlar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/2fa62262-2194-4c40-8f5a-c0a9218a3e41.png",
+            title: "Mujuskoy kapriz",
+            price: 31000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/103d2d96-3102-4033-8589-d8c100c4e274.png",
+            title: "Sezar",
+            price: 26000,
+        },
+    ],
+}
+
+const pitsa = {
+    pitsalar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/41f4b8ea-2bc9-461d-955c-5644419b92bb.png",
+            title: "Assorti pitsa",
+            price: 105000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/a910174e-e070-4b5f-b52c-2b363e1f64fe.png",
+            title: "Peperoni pitsa",
+            price: 90000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/b2d4335a-c35a-4cd8-aaa5-78b3124412b2.png",
+            title: "Go'shtli pitsa",
+            price: 115000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/031be6eb-7cc0-45ca-9895-f03ce748365c.png",
+            title: "Qazili pitsa",
+            price: 120000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/fbd2892a-b851-469b-9493-e1cf56f0d056.png",
+            title: "Tovuqli pitsa",
+            price: 95000,
+        },
+    ],
+}
+
+const doner = {
+    donerlar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/e18f6bee-0790-4f5e-a23d-8ba258f12a2d.png",
+            title: "Xaggi",
+            price: 36000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/ba94cc51-7a3f-4b25-b10b-509c64ad624f.png",
+            title: "Shourma",
+            price: 26000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/d87470f7-a2f5-469e-8ceb-41167015ade1.png",
+            title: "Pita doner",
+            price: 30000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/5eb594bd-979f-46c0-aaa6-ff6d126a25bb.png",
+            title: "Big doner",
+            price: 35000,
+        },
+    ],
+}
+
+const sneklar = {
+    sneklar: [
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/d4b94138-18c5-4fa2-8742-4f9e3b38fa4d.png",
+            title: "Kartoshka fri kichik",
+            price: 10000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/59f85f35-6749-4e1b-8ef5-d879e5364d3f.png",
+            title: "Jaydari kartoshka",
+            price: 16000,
+        },
+        {
+            img: "http://cc.oqtepalavash.uz/api/image/a62a2d40-c3f6-4306-977c-5383e74d6318.png",
+            title: "Non",
+            price: 4000,
+        },
+    ]
+}
+
+
+
+function createCard(item) {
+    return `
+        <div class="card">
+      
+            <img src="${item.img}" alt="${item.title}">
+            <h3>${item.title}</h3>
+            <p> ${item.price} So'm</p>
+            <button>Qo'shish</button>
+        </div>
+    `;
+}
+
+function renderCards(data, containerId) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = data.map(createCard).join('');
+    container.style.display = 'flex'; 
+}
+
+
+// document.querySelector('.b-1').addEventListener('click', function() {
+    renderCards(data.aksiyalar, 'aksiyalar-container');
+// });
+
+// Обработчик для кнопки "Barakali Setlar"
+ document.querySelector('.b-2').addEventListener('click', function() {
+    renderCards(barakalisetlar.barakaliSetlar, 'aksiyalar-container');
+   
+    
+});
+
+
+
+document.querySelector('.b-3').addEventListener('click', function() {
+    renderCards(lavashlar.lavashlar, 'aksiyalar-container');
+});
+
+
+document.querySelector('.b-4').addEventListener('click', function() {
+    renderCards(yahnakofee.yaxnaKofe, 'aksiyalar-container');
+});
+
+
+
+
+document.querySelector('.b-5').addEventListener('click', function() {
+    renderCards(bur.burgerHotDog, 'aksiyalar-container');
+});
+
+
+document.querySelector('.b-6').addEventListener('click', function() {
+    renderCards(pitsa.pitsalar, 'aksiyalar-container');
+});
+
+
+document.querySelector('.b-7').addEventListener('click', function() {
+    renderCards(salatlar.salatlar, 'aksiyalar-container');
+});
+
+document.querySelector('.b-8').addEventListener('click', function() {
+    renderCards(qarsildoq.sendvichJojalar, 'aksiyalar-container');
+});
+
+
+document.querySelector('.b-9').addEventListener('click', function() {
+    renderCards(doner.donerlar, 'aksiyalar-container');
+});
+
+
+document.querySelector('.b-10').addEventListener('click', function() {
+    renderCards(sneklar.sneklar, 'aksiyalar-container');
+});
+
+window.onload = function() {
+    setTimeout(function() {
+        var preloader = document.getElementById('preloader');
+        preloader.style.display = 'none';
+
+        var content = document.getElementById('content');
+        content.style.display = 'block';
+    }, 2000); // Задержка в 2 секунды
+};
+
